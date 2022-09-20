4d1d75a12c4e501ed8b2ea0f30804a42f9a37a3c
Lindseyhorn <lindseyhorn0512@gmail.com>
2022-09-20 17:29:18 +0200

    dark mode

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,15 @@
 // JavaScript Document
 console.log("hi");
+
+/* bron : css tips van de docent*/
+
+var darkMode
+
+darkMode = document.querySelector('button');
+deBody= document.querySelector('body');
+
+darkMode.addEventListener("click", verander);
+
+function verander() {
+    deBody.classList.toggle('dark_mode');
+}
