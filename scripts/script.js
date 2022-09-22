edf280d5711be28f5750d8da535d417d3b6c25bb
Lindseyhorn <lindseyhorn0512@gmail.com>
2022-09-22 11:58:40 +0200

    halloweenmode met body opmaak

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,9 @@ console.log("hi");
 
 /* bron : css tips van de docent*/
 
+
+/* darkmode*/
+
 var darkMode
 
 darkMode = document.querySelector('button');
@@ -13,5 +16,43 @@ darkMode.addEventListener("click", verander);
 function verander() {
     deBody.classList.toggle('dark_mode');
 }
+
+
+
+/* halloween thema tevoorschijn na op knop drukken*/
+
+var pumpkinKnop = document.querySelector('.pumpkinMode');
+console.log(pumpkinKnop);
+var pumpkinPlaatjes = document.querySelectorAll('.pumpkin');
+
+
+pumpkinKnop.addEventListener('click', function () {
+    pumpkinPlaatjes.forEach(pumpkin => {
+        console.log(pumpkin)
+        
+        pumpkin.classList.toggle('show')
+    });
+    console.log('clicked')
+})
+
+
+
+/* halloween thema*/
+
+halloween = document.querySelector('.pumpkinMode');
+deBody= document.querySelector('body');
+
+halloween.addEventListener("click", verander);
+
+function verander() {
+    deBody.classList.toggle('halloween_mode');
+}
+
+
+// var beweeg = document.querySelector("div[aria-label = 'beweeg']");
+// beweeg.addEventListener("click", heenEnWeer);
+// function heenEnWeer() {
+//   beweeg.classList.toggle("beweeg");
+// }
 
 
